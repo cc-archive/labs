194f2b823e522b3b6315016a8b1b10d0350c3485
Matt Lee <mattl@cnuk.org>
2014-12-10 12:09:41 -0500

    Remove generated files. Now in gitignore.

--- _site/demos/facebook/keys.php
+++
@@ -1,8 +0,0 @@
-<?
-
-// Change these to reflect your API and Application keys
-
-$appapikey = '4d0116929e941eb93b0e97bc5ff36215';
-$appsecret = '4c48752069a392b9a587334b36c62148';
-
-?>
--- _site/demos/search/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$query = $_GET['q'];
-
-header('Location: http://search.creativecommons.org/?query=' . $query);;
-
-
-?>
